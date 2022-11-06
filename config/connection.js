5aae8fab24b5fc90892c1a4054deb91c6598ea08
Darin VanAusdal <darinv@iMac2017.local>
2022-11-06 16:20:59 -0700

    connection to mongo database

---
+++ config/connection.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+// Wrap Mongoose around local connection to MongoDB
+  mongoose.connect('mongodb://127.0.0.1:27017/Mongoose', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+
+});
+
+
+
+// Export connection
+module.exports = mongoose.connection;
